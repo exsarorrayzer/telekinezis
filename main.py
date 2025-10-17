8c8138844c8561b52ac3adeac7a93140e6db17a1
exsarorrayzer <theleapy@gmail.com>
2025-10-17 21:52:58 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from colorama import Fore, Style, init
+from modules.backup_manager import BackupManager
+
+init(autoreset=True)
+
+def show_banner():
+    banner = f"""
+    {Fore.CYAN}╔══════════════════════════════════════════════╗
+    ║            {Fore.YELLOW}TELEKINEZIS BACKUP SYSTEM{Fore.CYAN}        ║
+    ║           {Fore.GREEN}♣︎{Fore.CYAN}          ║
+    ║                                             ║
+    ║              {Fore.MAGENTA}Author: exsarorrayzer{Fore.CYAN}          ║
+    ║                 {Fore.WHITE}Version: 2.1{Fore.CYAN}                ║
+    ╚══════════════════════════════════════════════╝{Style.RESET_ALL}
+    """
+    print(banner)
+
+def main():
+    show_banner()
+    backup_manager = BackupManager()
+    backup_manager.start()
+
+if __name__ == "__main__":
+    main()
